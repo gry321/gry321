fb254e478f760fa4cbdb685e5fd4c1da080be88c
gry321 <yao1127@aliyun.com>
2022-04-21 19:58:34 +0800

    Guoruoyao Info

---
+++ README.md
@@ -0,0 +1,16 @@
+### 我是郭若垚，是一个二（2）班的小学生
+里面有Java、Python的代码（其他语言没有出😓）
+使用[Lunicode](https://lunicode.com/bigtext "打开Lunicode的ASCII Art")
+                                                                                                                
+  ,ad8888ba,                             88888888ba                         8b        d8                        
+ d8"'    `"8b                            88      "8b                         Y8,    ,8P                         
+d8'                                      88      ,8P                          Y8,  ,8P                          
+88             88       88   ,adPPYba,   88aaaaaa8P'  88       88   ,adPPYba,  "8aa8"  ,adPPYYba,   ,adPPYba,   
+88      88888  88       88  a8"     "8a  88""""88'    88       88  a8"     "8a  `88'   ""     `Y8  a8"     "8a  
+Y8,        88  88       88  8b       d8  88    `8b    88       88  8b       d8   88    ,adPPPPP88  8b       d8  
+ Y8a.    .a88  "8a,   ,a88  "8a,   ,a8"  88     `8b   "8a,   ,a88  "8a,   ,a8"   88    88,    ,88  "8a,   ,a8"  
+  `"Y88888P"    `"YbbdP'Y8   `"YbbdP"'   88      `8b   `"YbbdP'Y8   `"YbbdP"'    88    `"8bbdP"Y8   `"YbbdP"'   
+                                                                                                                
+                                                                                                                
+                                                                                                                
+                                                                                                                
---
+++ info.java
@@ -0,0 +1,18 @@
+public class info {
+    public static void main(String []args){
+                String info = """
+                                                                                                                                       \s
+                          ,ad8888ba,                             88888888ba                         8b        d8                       \s
+                         d8"'    `"8b                            88      "8b                         Y8,    ,8P                        \s
+                        d8'                                      88      ,8P                          Y8,  ,8P                         \s
+                        88             88       88   ,adPPYba,   88aaaaaa8P'  88       88   ,adPPYba,  "8aa8"  ,adPPYYba,   ,adPPYba,  \s
+                        88      88888  88       88  a8"     "8a  88""\""88'    88       88  a8"     "8a  `88'   ""     `Y8  a8"     "8a \s
+                        Y8,        88  88       88  8b       d8  88    `8b    88       88  8b       d8   88    ,adPPPPP88  8b       d8 \s
+                         Y8a.    .a88  "8a,   ,a88  "8a,   ,a8"  88     `8b   "8a,   ,a88  "8a,   ,a8"   88    88,    ,88  "8a,   ,a8" \s
+                          `"Y88888P"    `"YbbdP'Y8   `"YbbdP"'   88      `8b   `"YbbdP'Y8   `"YbbdP"'    88    `"8bbdP"Y8   `"YbbdP"'  \s
+                                                                                                                                       \s
+                                                                                                                                       \s
+                """;
+        System.out.println(info);
+    }
+}
